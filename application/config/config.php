4ba4e58b961c0b01aa47ea28dc5bb5e23fb0d9b3
fajrullahalfian45 <64390753+fajrullahalfian45@users.noreply.github.com>
2020-08-04 19:19:01 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://fajrullahalfian45.github.io/test/';
+$config['base_url'] = 'https://myhorison.com';
 
 /*
 |--------------------------------------------------------------------------
